0829f91654fc8ee07213becd8c62c049179dc72d
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-03-18 00:29:07 +0600

    slider updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
-
 $(document).ready(function () {
     $('.center').slick({
         centerMode: true,
         arrows: true,
         dots: true,
-        centerPadding: '60px',
+        autoplay: true,
+        autoplaySpeed: 3000,
         slidesToShow: 5,
         responsive: [
-
             {
                 breakpoint: 1024,
                 settings: {
                     arrows: false,
                     centerMode: true,
-                    centerPadding: '40px',
                     slidesToShow: 3
                 }
             },
@@ -22,11 +20,37 @@ $(document).ready(function () {
                 settings: {
                     arrows: false,
                     centerMode: true,
-                    centerPadding: '40px',
                     slidesToShow: 1
                 }
             }
         ]
     });
 
+    // Function to update timer
+    function startSliderTimer() {
+        let timeLeft = 3; // Start from 3 seconds
+        let timerElement = $('<div class="slider-timer">3s</div>');
+
+        // Append timer to the current active slide
+        $('.mobilescreenslider').append(timerElement);
+
+        let interval = setInterval(() => {
+            timeLeft--;
+            if (timeLeft <= 0) {
+                timeLeft = 3; // Reset timer after 3s
+            }
+            $('.slider-timer').text(timeLeft + "s");
+        }, 1000);
+
+        $('.center').on('beforeChange', function () {
+            clearInterval(interval); // Stop old timer
+            $('.slider-timer').remove(); // Remove old timer
+        });
+
+        $('.center').on('afterChange', function () {
+            startSliderTimer(); // Restart timer for new slide
+        });
+    }
+
+    startSliderTimer(); // Start on load
 });
